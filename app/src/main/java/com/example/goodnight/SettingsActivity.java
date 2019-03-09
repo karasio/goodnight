ed958c6754d8c87a65fbcb12da99459d9284a28a
Katri Rasio <katri.rasio@metropolia.fi>
2019-03-09 20:19:42 +0200

    notification happens but wrong time

--- app/src/main/java/com/example/goodnight/SettingsActivity.java
+++ app/src/main/java/com/example/goodnight/SettingsActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.icu.util.Calendar;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +19,13 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import com.google.gson.Gson;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -27,16 +34,23 @@ public class SettingsActivity extends AppCompatActivity {
     private boolean cb_sleepTimeNotif;
     private boolean cb_logSleepNotif;
     private double time1, time2;
+    private int hour3, hour4, minute3, minute4;
 
     //Notification variables
     private int notification_logSleep = 1100;
     private int notification_bedtime = 1101;
     private NotificationHelper mNotificationHelper;
-
-    /*
-     * A view model for interacting with the UI elements.
-     */
-    //private MainUi mUIModel;
+    private Timer timer;
+    private TimerTask task;
+    private long currentTime;
+    private long day = 86400000;            // 24h in milliseconds
+    //private long day = 10000;                // debug toistoaika
+    private long bedTimeinMillis;
+    private long logSleepInMillis;
+    private long whichEverInMillis;
+    private Integer time1inMinutes = 0;
+    private Integer time2inMinutes = 0;
+    private int whenToNotify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,24 +62,35 @@ public class SettingsActivity extends AppCompatActivity {
         picker4.setIs24HourView(true);
 
         mNotificationHelper = new NotificationHelper(this);
+        timer = new Timer();
+
         //mUIModel = new MainUi(findViewById(R.id.activity_settings));
     }
 
     public void saveButtonPressed(View view) {
 
-        int hour3 = picker3.getHour();
-        int minute3 = picker3.getMinute();
+        hour3 = picker3.getHour();
+        minute3 = picker3.getMinute();
 
-        int hour4 = picker4.getHour();
-        int minute4 = picker4.getMinute();
+        hour4 = picker4.getHour();
+        minute4 = picker4.getMinute();
 
         time1 = hour3 + minute3/60.0;
-        time2 = hour4 + minute4/60.0;
+        time1inMinutes = hour3 * 60 + minute3;
+        Log.d("notski", "bedtime " + time1);
 
+        time2 = hour4 + minute4/60.0;
+        time2inMinutes = hour4 * 60 + minute4;
 
         if (view.getId() == R.id.button_saveSettings) {
+
+            //Log.d("notski", "sleepnotif " + cb_sleepTimeNotif + " logNotif "+ cb_logSleepNotif);
+
+            // set up notifications
+            notificationAtCertainTime();
+
+            // store data and finish
             DataHandler.getInstance().setSettings(cb_sleepTimeNotif, time1, cb_logSleepNotif, time2);
-            Log.d("kake", "sleepnotif " + cb_sleepTimeNotif + " logNotif "+ cb_logSleepNotif);
             finish();
         }
     }
@@ -79,15 +104,15 @@ public class SettingsActivity extends AppCompatActivity {
             case R.id.checkBox_bedtimeNotif:
                 if (checked) {
                     cb_sleepTimeNotif = true;
-                    sendNotification(notification_bedtime);
                 } else {
                     cb_sleepTimeNotif = false;
+                    task.cancel();
                 }
                 break;
             case R.id.checkBox_logSleepNotif:
                 if (checked) {
                     cb_logSleepNotif = true;
-                    sendNotification(notification_logSleep);
+
                 } else {
                     cb_logSleepNotif = false;
                 }
@@ -110,6 +135,7 @@ public class SettingsActivity extends AppCompatActivity {
     public void resetButtonPressed(View view) {
         DataHandler.getInstance().eraseNights();
         saveNights();
+        task.cancel();
         Log.d("appi", "Data erased");
         finish();
     }
@@ -132,7 +158,7 @@ public class SettingsActivity extends AppCompatActivity {
         if (id == notification_logSleep) {
             notificationBuilder =
                     mNotificationHelper.getNotificationLogSleep(
-                            getString(R.string.notification_channel_goodnight),
+                            getString(R.string.notification_common),
                             getString(R.string.notification_logSleep));
         }
         if (notificationBuilder != null) {
@@ -140,73 +166,95 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    /** Send Intent to load system Notification Settings for this app. */
-    private void goToNotificationSettings() {
-        Intent i = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-        i.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
-        startActivity(i);
+    public void notificationAtCertainTime() {
+        if (cb_sleepTimeNotif || cb_logSleepNotif) {
+            if (cb_sleepTimeNotif && cb_logSleepNotif) {
+                task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (cb_sleepTimeNotif) {
+
+                            bedTimeinMillis = new Long(time1inMinutes);
+                            currentTime = System.currentTimeMillis();
+                            bedTimeinMillis = currentTime + (bedTimeinMillis * 60000);
+                            Log.d("notski", "BD notif in millis " + bedTimeinMillis + " currenttime " + currentTime);
+
+                            bedTimeinMillis = bedTimeinMillis + currentTime;
+                            sendNotification(notification_bedtime);
+                            //Log.d("notski", "BD notif in millis " + bedTimeinMillis + " currenttime " + currentTime);
+                        }
+                        if (cb_logSleepNotif) {
+
+                            logSleepInMillis = new Long(time2inMinutes);
+                            currentTime = System.currentTimeMillis();
+                            logSleepInMillis = currentTime + (logSleepInMillis * 60000);
+                            Log.d("notski", "LS notif in millis " + logSleepInMillis + " currenttime " + currentTime);
+                            sendNotification(notification_logSleep);
+                        }
+                    }
+                };
+                Log.d("notski", "both notifications set");
+                timer.scheduleAtFixedRate(task, bedTimeinMillis, day);
+                timer.scheduleAtFixedRate(task, logSleepInMillis, day);
+            }
+
+            ///////////////////////
+            // DEBUG THIS ONE & ONCE IT WORKS, COPY TO OTHERS!!
+            //////////////////////
+            else if (cb_sleepTimeNotif || cb_logSleepNotif) {
+                task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (cb_sleepTimeNotif) {
+
+                            int hoursNOW =Calendar.HOUR_OF_DAY + 8;                // timezone difference
+                            int minutesNOW = Calendar.MINUTE;
+                            Log.d("notski", hoursNOW + ":" + minutesNOW);
+
+                            int timeNow = hoursNOW*60 + minutesNOW;
+                            Log.d("notski", "TimeNow " + timeNow);
+
+                            if (timeNow > time1inMinutes) {
+                                whenToNotify = (timeNow - time1inMinutes + 1440) * 60000;     // notification wanted next time tomorrow & in milliseconds
+
+                            } else {
+                                whenToNotify = (time1inMinutes - timeNow) * 60000;
+                            }
+                            Log.d("notski", "timedifference in milliseconds " + whenToNotify);
+                            whichEverInMillis = new Long(whenToNotify);
+                            Log.d("notski", "whichEverInMillis " + whichEverInMillis);
+
+                            sendNotification(notification_bedtime);
+//                            time1inMinutes = time1inMinutes * 60000;
+//                            whichEverInMillis = Long.valueOf(time1inMinutes.longValue());
+//                            Log.d("notski", "time1inminutes as ms&long " + whichEverInMillis);
+//                            currentTime = System.currentTimeMillis();
+//                            Log.d("notski", "currentTime " + currentTime);
+//                            currentTime += whichEverInMillis;
+//                            //Log.d("notski", "bedtime notif set");
+//                            Log.d("notski", "notif at " + currentTime);
+
+                        }
+                        ///////////////////
+                        //END DEBUG
+                        //////////////////
+                        if (cb_logSleepNotif) {
+                            sendNotification(notification_logSleep);
+                            whichEverInMillis = new Long(time2inMinutes);
+                            currentTime = System.currentTimeMillis();
+                            whichEverInMillis = currentTime + (whichEverInMillis * 60000);
+                            Log.d("notski", "logsleep notif set");
+                            Log.d("notski", "LS notif in millis " + whichEverInMillis + " currenttime " + currentTime);
+
+                        }
+                    }
+                };
+                timer.scheduleAtFixedRate(task, 300000, day);
+            } else {
+                Log.d("notski", "no notifications set");
+            }
+        }
     }
-
-    /**
-     * Send intent to load system Notification Settings UI for a particular channel.
-     *
-     * @param channel Name of channel to configure
-     */
-    private void goToNotificationChannelSettings(String channel) {
-        // Skeleton method to be completed later
-        Intent i = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-        i.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
-        startActivity(i);
-    }
-
-    /**
-     * View model for interacting with Activity UI elements. (Keeps core logic for sample separate.)
-     */
-//    class MainUi implements View.OnClickListener {
-//
-//        private MainUi(View root) {
-//
-//            // Setup the buttons
-//            (root.findViewById(R.id.birthday_follower_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.life_follower_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.follower_channel_settings_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.friend_dm_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.coworker_dm_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.dm_channel_settings_button)).setOnClickListener(this);
-//            (root.findViewById(R.id.go_to_settings_button)).setOnClickListener(this);
-//        }
-//
-//        @Override
-//        public void onClick(View view) {
-//            switch (view.getId()) {
-//                case R.id.birthday_follower_button:
-//                    sendNotification(NOTIFICATION_FOLLOW);
-//                    break;
-//                case R.id.life_follower_button:
-//                    sendNotification(NOTIFICATION_UNFOLLOW);
-//                    break;
-//                case R.id.follower_channel_settings_button:
-//                    goToNotificationChannelSettings("");
-//                    break;
-//                case R.id.friend_dm_button:
-//                    sendNotification(NOTIFICATION_DM_FRIEND);
-//                    break;
-//                case R.id.coworker_dm_button:
-//                    sendNotification(NOTIFICATION_DM_COWORKER);
-//                    break;
-//                case R.id.dm_channel_settings_button:
-//                    goToNotificationChannelSettings("");
-//                    break;
-//                case R.id.go_to_settings_button:
-//                    goToNotificationSettings();
-//                    break;
-//                default:
-//                    Log.e(TAG, getString(R.string.error_click));
-//                    break;
-//            }
-//        }
-//    }
-
 }
 
 
